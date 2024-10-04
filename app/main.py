74f659e0e1d4c28507a490fe1d62da72fdb85459
Alexander Balaban <balaban.alexander+github@gmail.com>
2024-10-04 23:18:23 +0100

    fs isolation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+import os
+import shutil
 import subprocess
 import sys
+import tempfile
 
 
 def subprocess_run(argv: list[str]):
@@ -7,10 +10,14 @@ def subprocess_run(argv: list[str]):
     
 
 def main():
-    completed = subprocess_run(sys.argv[3:])
-    sys.stdout.buffer.write(completed.stdout)
-    sys.stderr.buffer.write(completed.stderr)
-    sys.exit(completed.returncode)
+    bin_path = sys.argv[2]
+    with tempfile.TemporaryDirectory() as dir:
+        shutil.move(bin_path, os.path.join(dir, bin_path))
+        os.chroot(dir)
+        completed = subprocess_run(sys.argv[3:])
+        sys.stdout.buffer.write(completed.stdout)
+        sys.stderr.buffer.write(completed.stderr)
+        sys.exit(completed.returncode)
 
 
 if __name__ == "__main__":
