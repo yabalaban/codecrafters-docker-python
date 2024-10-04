56f8e5cf83a3b81d1ce4d38938fc0f6c2c55ad92
Alexander Balaban <balaban.alexander+github@gmail.com>
2024-10-04 23:00:41 +0100

    local run

--- app/main.py
+++ app/main.py
@@ -2,17 +2,14 @@ import subprocess
 import sys
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+def run_local(argv: list[str]):
+    completed_process = subprocess.run(argv, capture_output=True)
+    return completed_process.stdout.decode("utf-8")
 
-    # Uncomment this block to pass the first stage
-    #
-    # command = sys.argv[3]
-    # args = sys.argv[4:]
-    #
-    # completed_process = subprocess.run([command, *args], capture_output=True)
-    # print(completed_process.stdout.decode("utf-8"))
+
+def main():
+    local_res = run_local(sys.argv[3:])
+    print(local_res)
 
 
 if __name__ == "__main__":
